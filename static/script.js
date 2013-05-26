46dc5cffb22f170b779be43f4104de3289dde707
Alessio <alessio.dellamotta@gmail.com>
2013-05-26 16:02:15 +0200

    Added some kind of D3 support

--- static/script.js
+++ static/script.js
@@ -1,5 +1,22 @@
 jQuery(document).ready(function($){
 
+  var width = 800, height = 600;
+
+  var cluster = d3.layout.cluster()
+      .size([height, width - 160]);
+
+  var diagonal = d3.svg.diagonal()
+      .projection(function (d) {
+      return [d.y, d.x];
+  });
+
+  var svg = d3.select("body").append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .append("g")
+      .attr("transform", "translate(40,0)");
+
+
   $("#searchForm").submit(function(event) {
     event.preventDefault();
     var $form = $( this ),
@@ -10,10 +27,45 @@ jQuery(document).ready(function($){
     var posting = $.post( url, { search_string: str, secret: s3cret } );
     
     posting.done(function( data ) {
-      //var content = $( data ).find( '#content' );
-      //$( "#result" ).empty().append( content );
       $( "#result" ).empty().append( data );
-    });
-  });
 
-});
+      data = JSON.parse(data);
+      var nodes = cluster.nodes(data["crawled_paths"][0]),
+          links = cluster.links(nodes);
+
+      var link = svg.selectAll(".link")
+          .data(links)
+          .enter().append("path")
+          .attr("class", "link")
+          .attr("d", diagonal);
+
+      var node = svg.selectAll(".node")
+          .data(nodes)
+          .enter().append("g")
+          .attr("class", "node")
+          .attr("transform", function (d) {
+              return "translate(" + d.y + "," + d.x + ")";
+          });
+
+      node.append("circle")
+          .attr("r", 10);
+
+      node.append("text")
+          .attr("dx", function (d) {
+              return d.children ? 8 : 15;
+          })
+          .attr("dy", function (d){
+              return d.children ? -20 : 3;
+          })
+          .style("text-anchor", function (d) {
+              return d.children ? "end" : "start";
+          })
+          .text(function (d) {
+              return d.name;
+          });
+
+    }); // post done
+
+  }); // serch form
+
+}); // jQuery doc on load
